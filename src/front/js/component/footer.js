6164f5a783f2dd9962ec5a9bab3b39cf62958f25
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-11-23 20:09:52 +0000

    links, mail and copyright

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import "../../styles/pages/footer.scss";
 
 export const Footer = () => (
 	<footer className="py-5 text-center text-light bg-dark sticky-bottom">
-		<p>&#169; Pablo, Jaime & Clara. All rigths reserved</p>
+		<p>&#169; Pablo, Jaime & Clara. 2021 All rigths reserved</p>
+		<p>example@example.com</p>
+		<a href="https://www.facebook.com/" className="fab fa-instagram footer-link" />
+		<a href="https://www.instagram.com/" className="fab fa-facebook-square footer-link" />
+		<a href="https://twitter.com/" className="fab fa-twitter footer-link" />
 	</footer>
 );
