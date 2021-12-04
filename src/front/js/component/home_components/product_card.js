b6afc4be3cdafe61de1850bf052fadb5a480d4ec
Javier <jmanteca@jmanvel.com>
2021-12-04 12:12:53 +0000

    pagina info a falta de imagen

--- src/front/js/component/home_components/product_card.js
+++ src/front/js/component/home_components/product_card.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import manicura from "./../../../img/manicura.jpg";
 import PropTypes from "prop-types";
+import { Context } from "../../store/appContext";
+import { Link } from "react-router-dom";
 
 export const Product_card = props => {
+	const { actions, store } = useContext(Context);
+
 	return (
 		<div className="card m-3" style={{ width: "18rem" }}>
 			<img src={manicura} className="card-img-top" alt="..." style={{ width: "18rem" }} />
 			<div className="card-body">
 				<h5 className="card-title">{props.product.name}</h5>
 				<p className="card-text">{props.product.description}</p>
-				<a href="#" className="btn btn-success mt-2">
-					Más info
-				</a>
+				<button
+					className="btn btn-success mt-2"
+					onClick={() => {
+						actions.getProduct(props.product.id);
+					}}>
+					<Link to={"/info/" + props.product.id}>
+						<span className="text-whit ">Más Info</span>
+					</Link>
+				</button>
 			</div>
 		</div>
 	);
