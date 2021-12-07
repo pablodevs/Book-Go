31fb95b2c7bc8d7ddd472a9facd9a91235ea07f1
peibol888 <pabloalamovargas@gmail.com>
2021-12-07 14:53:55 +0000

    navbar finished

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,32 +5,34 @@ import "../../styles/components/navbar.scss";
 export const Navbar = () => {
 	let [menu, setMenu] = useState(false);
 	return (
-		<nav className="_navbar">
-			<span className="_navbar-logo-wrapper center">
-				<NavLink className="_navbar-logo _navbar-link" to="/" onClick={() => setMenu(false)}>
-					Inicio
-				</NavLink>
-			</span>
-			<button className="_navbar-login">
-				<div className="_navbar-login-effect">
-					<span>Acceder</span>
-					<i className="far fa-user" />
-				</div>
-			</button>
-			<button className="_navbar-toggle" onClick={() => setMenu(!menu)}>
-				{menu ? <i className="fas fa-times" /> : <i className="fas fa-bars" />}
-			</button>
-			<ul className={"_navbar-group" + (menu ? " _navbar-show" : "")}>
-				<NavLink className="_navbar-link" to="/calendar" onClick={() => setMenu(false)}>
-					Reserva
-				</NavLink>
-				<NavLink className="_navbar-link" to="#" onClick={() => setMenu(false)}>
-					Productos
-				</NavLink>
-				<NavLink className="_navbar-link" to="#" onClick={() => setMenu(false)}>
-					Contacto
-				</NavLink>
-			</ul>
-		</nav>
+		<header>
+			<nav className="_navbar">
+				<span className="_navbar-logo-wrapper center">
+					<NavLink className="_navbar-logo _navbar-link" to="/" onClick={() => setMenu(false)}>
+						Inicio
+					</NavLink>
+				</span>
+				<button className="_navbar-login">
+					<div className="_navbar-login-effect">
+						<span>Acceder</span>
+						<i className="far fa-user" />
+					</div>
+				</button>
+				<button className="_navbar-toggle" onClick={() => setMenu(!menu)}>
+					{menu ? <i className="fas fa-times" /> : <i className="fas fa-bars" />}
+				</button>
+				<ul className={"_navbar-group" + (menu ? " _navbar-show" : "")}>
+					<NavLink className="_navbar-link" to="/calendar" onClick={() => setMenu(false)}>
+						Reserva
+					</NavLink>
+					<NavLink className="_navbar-link" to="#" onClick={() => setMenu(false)}>
+						Productos
+					</NavLink>
+					<NavLink className="_navbar-link" to="#" onClick={() => setMenu(false)}>
+						Contacto
+					</NavLink>
+				</ul>
+			</nav>
+		</header>
 	);
 };
