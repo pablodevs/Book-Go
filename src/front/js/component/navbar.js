29a9057832ee09d97b8adc4dc3202599e91a7407
peibol888 <pabloalamovargas@gmail.com>
2022-01-25 10:38:14 +0000

    ...

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -28,26 +28,34 @@ export const Navbar = () => {
 					</NavLink>
 				</span>
 				{store.token ? (
-					<Link to={store.user.is_admin ? "/admin/welcome" : "/dashboard/welcome"}>
-						{store.user.img_url ? (
-							<img
-								className="_navbar-profile-img"
-								src={store.user.img_url}
-								alt="foto_perfil"
-								width="30"
-								height="30"
-							/>
-						) : (
-							<div className="avatar avatar-miniature">
-								<svg viewBox="0 0 24 24" className="avatar__img">
-									<path
-										d="M12,3.5c2.347,0,4.25,1.903,4.25,4.25S14.347,12,12,12s-4.25-1.903-4.25-4.25S9.653,3.5,12,3.5z
+					<Link
+						to={store.user.is_admin ? "/admin/welcome" : "/dashboard/welcome"}
+						className="_navbar-profile-wrapper">
+						<div>
+							{store.user.img_url ? (
+								<img
+									className="_navbar-profile-img"
+									src={store.user.img_url}
+									alt="foto_perfil"
+									width="30"
+									height="30"
+								/>
+							) : (
+								<div className="avatar avatar-miniature">
+									<svg viewBox="0 0 24 24" className="avatar__img">
+										<path
+											d="M12,3.5c2.347,0,4.25,1.903,4.25,4.25S14.347,12,12,12s-4.25-1.903-4.25-4.25S9.653,3.5,12,3.5z
                                 M5,20.5
                                 c0-3.866,3.134-7,7-7s7,3.134,7,7H5z"
-									/>
-								</svg>
-							</div>
-						)}
+										/>
+									</svg>
+								</div>
+							)}
+						</div>
+						<div>
+							<span>Perfil</span>
+							<i className="fas fa-chevron-down" />
+						</div>
 					</Link>
 				) : (
 					<button
